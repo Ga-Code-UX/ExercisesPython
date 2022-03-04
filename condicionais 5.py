86410e2e1d779e1cd5c87581501d482a094edbae
Ge-makeralgorithm <100951366+Ge-makeralgorithm@users.noreply.github.com>
2022-03-04 16:08:02 +0000

    Add files via upload
    python

---
+++ condicionais 5.py
@@ -0,0 +1,10 @@
+# Faça um programa que receba um número inteiro e verifica se ele é par ou impar
+X = int(input(" Digite um número:"))
+if X %2 != 0:
+  print (" Esse número é impar ")
+else:
+  print (" Esse número é par")
+
+
+
+
